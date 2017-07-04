9073f80b4eabf9399c6baade74fc7bd3892fe562
Smriti Shyamal <smritishyamal123@gmail.com>
2017-07-03 20:49:21 -0400

    Update spark_amazon.scala

--- spark_amazon.scala
+++ spark_amazon.scala
@@ -39,3 +39,31 @@ println("Writing to text file")
 printToFile(new File("/resources/data/amazon0302.txt")) { p =>
   iterator.foreach(p.println)
 }
+
+// Import GraphX and all of its components
+import org.apache.spark.graphx._
+
+// Create the graph
+val amazonGraph = GraphLoader.edgeListFile(sc=sc,
+                                           path="/resources/data/amazon0302.txt",
+                                           canonicalOrientation=true).partitionBy(PartitionStrategy.RandomVertexCut)
+
+// Number of vertices and edges of our amazonGraph
+println("Number of vertices: " + amazonGraph.numVertices)
+println("Number of edges: " + amazonGraph.numEdges)
+
+// Cache the graph for faster computations
+amazonGraph.cache()
+
+// To analyze more, we can find those products that are frequently co-purchased together and form a "cluster" in order to,
+// for example, recommend "related" products when someone buys a product or even balance the supply-demand, etc. 
+// There are number of ways we can define what we mean by a cluster in a graph. The simplest cluster is a triangle.
+val triCounts = amazonGraph.triangleCount()
+
+// The result of calling triCounts.vertices is an RDD of pairs of (VertexId, # triangles containing the Vertex). 
+// Therefore, we can count the total number of triangles, by suming up all the each vertex triangle count with reduce. 
+// Moreover, since each edge is counted three times (once for every vertex of a triangle), we need to divide by  3
+// to find the exact total count. The result in fact, verifies the claim here about the number of triangles.
+val totalTriCounts = triCounts.vertices.map(x => x._2).reduce(_ + _) / 3
+println("Total number of triangles: " + totalTriCounts)
+
